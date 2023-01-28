de5d1d74ba8bcdb2986e1e8af3533726d2aee15d
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2023-01-28 17:04:17 -0500

    changes to about me

---
+++ build/precache-manifest.f686d545fe7218e8d598b143ce9afe2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99497ff5477caa7b07ac88a82a7db3e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e6b1dd823d01df8328c",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "5913f7c7f12c110ed0f0",
+    "url": "/static/css/main.b28e7c8b.chunk.css"
+  },
+  {
+    "revision": "4e6b1dd823d01df8328c",
+    "url": "/static/js/2.afaee287.chunk.js"
+  },
+  {
+    "revision": "5913f7c7f12c110ed0f0",
+    "url": "/static/js/main.a8a1bb37.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  },
+  {
+    "revision": "f9e678a84e712851e5d2c73f2f752de3",
+    "url": "/static/media/olukukoyiresume.f9e678a8.pdf"
+  }
+]);
--- src/SkillList.js
+++ src/SkillList.js
@@ -14,10 +14,10 @@ function SkillList(props) {
         <h1>Hey i'm Olu</h1>
         <h3>Heres a bit about me....</h3>
         <p>
-          I am a frontend web developer, and publishing industries for
-          previewing layouts and visual mockups.Lorem ipsum is placeholder text
-          commonly used in the graphic, print, and publishing industries for
-          previewing layouts and visual mockups.
+          I am Olu Kukoyi, a sophomore at The City College of New York pursuing
+          a bachelors degree in computer science. I have experience as a
+          Front-End/Full-stack Developer. I enjoy working with all layers of the
+          stack and building user friendly and efficient web applications!
         </p>
       </div>
       <div className="skills__list">
